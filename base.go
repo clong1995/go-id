5bf15b127764f22286bd46de2cb981fc536f7cb7
huazhimeng <2902151499@qq.com>
2025-02-20 10:15:24 +0800

    提交

--- base.go
+++ base.go
@@ -35,6 +35,29 @@ func Encode(num int64) string {
 	return string(result[:index])
 }
 
+// EncodeId 使用id加密
+func EncodeId(num, uid int64) string {
+	if num == 0 && uid == 0 {
+		return string(chars[0])
+	}
+	return Encode(num + uid)
+}
+
+// EncodeNoXor 非xor编码
+func EncodeNoXor(num int64) string {
+	if num == 0 {
+		return string(chars[0])
+	}
+	var result [8]rune
+	index := 0
+	for num > 0 {
+		result[index] = chars[num&0xFF]
+		num >>= 8
+		index++
+	}
+	return string(result[:index])
+}
+
 // Decode 解码
 func Decode(encoded string) (result int64) {
 	runes := []rune(encoded)
@@ -58,22 +81,16 @@ func Decode(encoded string) (result int64) {
 			result = result<<8 | maps[char]
 		}
 	}
-	return result
+	return
 }
 
-// EncodeNoXor 非xor编码
-func EncodeNoXor(num int64) string {
-	if num == 0 {
-		return string(chars[0])
-	}
-	var result [8]rune
-	index := 0
-	for num > 0 {
-		result[index] = chars[num&0xFF]
-		num >>= 8
-		index++
-	}
-	return string(result[:index])
+// DecodeId 使用id解密
+func DecodeId(encoded string, uid int64) (result int64) {
+	dId := Decode(encoded)
+	//提取最高位
+	//highestDigit := int64(float64(dId) / math.Pow10(int(math.Log10(float64(dId))))) // 用 log10 计算位数，再除以 10 的次幂
+	result = dId - uid
+	return
 }
 
 func xorKey() (key int64) {
